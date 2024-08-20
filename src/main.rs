e7662a39d8c8bde6968d12f5d2e83f4d29d245ba
Jakson Vermillion <1000dimensions@github.com>
2024-08-19 21:34:49 -0400

    Some nice additions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
-use std::io;
 use std::io::*;
 use std::fs::{File, Permissions, OpenOptions};
+use std::string::String;
 
 fn main() {
     App::new()
@@ -27,13 +27,13 @@ fn readfile(b: bool) -> bool{
         let mut c = String::new();
         ile.read_to_string(&mut c);
         if c == "" {
-            ile.write_all(b"0\n");
+            ile.write_all(b"0");
             return false;
         }
-        if c == "0\n"{
+        if c == "0"{
             return false;
         }
-        if c == "1\n"{
+        if c == "1"{
             return true;
         }
         else {
@@ -47,7 +47,7 @@ fn readfile(b: bool) -> bool{
                             .open("src/.b")
                             .unwrap();
         let meta = ile.metadata().unwrap();
-        ile.write_all(b"1\n");
+        ile.write_all(b"1");
         return true;
     }
     else {
@@ -67,18 +67,20 @@ fn write() {
             break;
         }
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("failed to read the line. Maybe try something valid");
+        stdin().read_line(&mut guess).expect("failed to read the line. Maybe try something valid");
         m = m + 1;
+        guess = guess.to_lowercase().replace(['?', '!', '@', '#', '"',',', ' ', '/', '$', '%', '^', '&', '*', '(', ')', '_', '-','+', '=', '.', ';', ':','\'','>', '<', '`', '~', '|', '\\', '{','}','[',']','(',')'], "");
+        guess = guess.trim().to_string();
         let name = guess.clone();
         let g = respond(guess, m, intime);
         println!("{:?}", g);
-        if name == "Yourmom\n"{
-            println!("You're banned.");
+        if name == "yourmom"{
+            println!("Your banned.");
             let ban = readfile(true);
             break;
         }
         if m == 33 || m == 34 || m == 35 || m == 36 || m == 37 || m == 38 || m == 39{
-            if name == "Savior\n" {
+            if name == "savior" {
                 if intime == true {
                     intime = true;
                 }
@@ -91,11 +93,11 @@ fn write() {
         if m >= 40 {
             intime = false;
         }
-        if name == "John\n"{
+        if name == "john"{
             println!("It took you {:?} tries", m);
             break;
         }
-        if name == "Youremom\n"{
+        if name == "youremom"{
             println!("You're banned.");
             let ban = readfile(true);
             break;
@@ -105,7 +107,7 @@ fn write() {
 }
 
 fn respond(response: String, tries: i32, intime: bool) -> &'static str{
-    if response == "John\n"{
+    if response == "john"{
         if tries == 1 {
             return "You are cheating aren't you? No one gets this first try.";
         }
@@ -114,58 +116,58 @@ fn respond(response: String, tries: i32, intime: bool) -> &'static str{
 
         }
     }
-    if response == "Farside\n" { 
+    if response == "farside" { 
         return "Great comic! One of my favorites deals with Dear John.";
     }
-    if response == "Dearjohn\n" {
+    if response == "dearjohn" {
         return "I haven't got one of these yet! Granted I may because my name is in it."
     }
-    if response == "Gander\n" {
+    if response == "gander" {
         return "Yes, you take one of these. No stealing though";
     }
-    if response == "Stealing\n" {
+    if response == "stealing" {
         return "Don't.";
     }
-    if response == "Dont\n" {
+    if response == "dont" {
         return "It's a conjuntion of do and not. Yoda says this non conjucntioned version in his qoute 'do or do not there is no try'";
     }
-    if response == "Yoda\n"{
+    if response == "yoda"{
         return "Wrong path you have gone down hmm. Different path you must take."
     }
-    if response == "Thor\n" {
+    if response == "thor" {
         return "GREAT PROGRAMMER. Love this man. He is a great movitvator."
     }
-    if response == "Failboat\n" {
+    if response == "failboat" {
         return "The idea is simple."
     }
-    if response == "Jaymoji\n"{
+    if response == "jaymoji"{
         return "That guy who yells BOMB! a lot."
     }
-    if response == "Yourmom\n"{
+    if response == "yourmom"{
         return "I am banning you."
     }
-    if response == "Dear\n" {
+    if response == "dear" {
         return "Why do you think my name is Dear?";
     }
-    if response == "Wallace\n"{
+    if response == "wallace"{
         return "The Brave? Great comic. Farside is great."
     }
-    if response == "Calvin\n"{
+    if response == "calvin"{
         return "AND HOBBBES!"
     }
-    if response == "Hobbes\n"{
-        return "Calvin and Hobbes is great. Reminds me of Wallace."
+    if response == "hobbes"{
+        return "Calvin and Hobbes is great. Reminds me of Wallace The Brave."
     }
-    if response == "Again\n"{
+    if response == "again"{
         return "Very clever... Suppose you get yourself a hint. Calvin."
     }
-    if response == "Brave\n"{
+    if response == "brave"{
         return "I am not brave. Sorry"
     }
-    if response == "Sorry\n"{
+    if response == "sorry"{
         return "You will be."
     }
-    if response == "Thetruth\n" {
+    if response == "thetruth" {
         if tries >= 10 {
             return "You seek what may be most deadly. I send you on a quest."
         }
@@ -173,7 +175,7 @@ fn respond(response: String, tries: i32, intime: bool) -> &'static str{
             return "Don't try to find it, for your sake."
         }
     }
-    if response == "Quest\n" {
+    if response == "quest" {
         if tries >= 10{
             return "The pen is greater than the sword. Many have tried to find the truth but have not."
         }
@@ -181,7 +183,7 @@ fn respond(response: String, tries: i32, intime: bool) -> &'static str{
             return "Yes you're on one to find the name."
         }
     }
-    if response == "Pen\n" {
+    if response == "pen" {
         if tries >= 10{
             return "The truth is the killer of gods. It is powerful enough to destroy planets"
         }
@@ -189,7 +191,7 @@ fn respond(response: String, tries: i32, intime: bool) -> &'static str{
             return "An elegant writing utensil."
         }
     }
-    if response == "Planets\n"{
+    if response == "planets"{
         if tries >= 10 {
             return "And yet you still wish to seek it. I cannot tell you must seek."
         }
@@ -197,7 +199,7 @@ fn respond(response: String, tries: i32, intime: bool) -> &'static str{
             return "YOU LIVE ON ONE!! YAY!!!"
         }
     }
-    if response == "Seek\n" {
+    if response == "seek" {
         if tries <= 30 {
             return "You will not find it at this time. I refuse you."
         }
@@ -209,7 +211,7 @@ fn respond(response: String, tries: i32, intime: bool) -> &'static str{
             return "Are you still looking for my name?"
         }
     }
-    if response == "Universe\n" {
+    if response == "universe" {
         if tries == 32 { 
             return "The truth is complex. Depending on which path you take the truth is different."
         }
@@ -217,12 +219,15 @@ fn respond(response: String, tries: i32, intime: bool) -> &'static str{
             return "Good grief you think the answer is the universe? Try 'again'"
         }
     }
-    if response == "Complex\n" {
+    if response == "complex" {
         if tries == 33 {
             return "Like all things are. From mistakes of the past we must try again"
         }
+        else {
+            return "You think this is complex? Really?"
+        }
     }
-    if response == "Path\n" {
+    if response == "path" {
         if tries == 32 {
             return "The truth is strange. It is both the savior and death of life."
         }
@@ -230,7 +235,10 @@ fn respond(response: String, tries: i32, intime: bool) -> &'static str{
             return "You need to look for one. Hop to it."
         }
     }
-    if response == "Savior\n"{
+    if response == "anything"{
+        return "Nothing."
+    }
+    if response == "savior"{
         if intime == true{
             if tries == 33 {
                 return "I'm sorry you will not find a savior in the truth."
@@ -261,22 +269,22 @@ fn respond(response: String, tries: i32, intime: bool) -> &'static str{
             return "No I'm no savior. "
         }
     }
-    if response == "Way\n"{
+    if response == "way"{
         return "When is there is a will there is one of these."
     }
-    if response == "Will"{
+    if response == "will"{
         return "There are many wills. Like the will to live. The will to eat. The will to code nonstop."
     }
-    if response == "Live"{
+    if response == "live"{
         return "BREAKING NEWS! WE GO LIVE TO JOHN WHO IS ON THE SCENE!"
     }
-    if response == "Eat"{
+    if response == "eat"{
         return "Great now you made me hungry. Come on man."
     }
-    if response == "Nonstop"{
+    if response == "nonstop"{
         return "Would put a song here. But I don't want to have copyright issues"
     }
-    if response == "Youremom\n"{
+    if response == "youremom"{
         return "Good job spelling. Your're still banned."
     }
     else {
